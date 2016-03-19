cf3c2eccf68b3d613277e7976554540e516982fb
Saquib Hafiz <hafiz.saquib@gmail.com>
2016-03-19 14:03:56 -0400

    lots of changes

--- getEvenOddHarmonicsRatio.m
+++ getEvenOddHarmonicsRatio.m
@@ -1,6 +1,6 @@
 function evenOddHarmonicsRatio = getEvenOddHarmonicsRatio(harmonics)
     evenHarmonics = harmonics(2:2:length(harmonics));
-    oddHarmonics = harmonics(3:2:length(harmonics));
+    oddHarmonics = harmonics(1:2:length(harmonics));
     evenOddHarmonicsRatio = sum(evenHarmonics) / sum(oddHarmonics);
-    evenOddHarmonicsRatio = evenOddHarmonicsRatio / harmonics(1);
+%     evenOddHarmonicsRatio = evenOddHarmonicsRatio / harmonics(1);
 end
