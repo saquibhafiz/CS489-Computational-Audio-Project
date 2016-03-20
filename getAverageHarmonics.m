29d7b2aa12a8c9b9db0faeeaae5f325e820b28b4
Saquib Hafiz <hafiz.saquib@gmail.com>
2016-03-20 15:03:47 -0400

    got rid of  commented lines and moved the normalization line to an appropriate place

--- getAverageHarmonics.m
+++ getAverageHarmonics.m
@@ -15,18 +15,6 @@ function [averageHarmonics, averageSound] = getAverageHarmonics(fileName, note)
         soundMatrix = soundMatrix';
         soundMatrix = soundMatrix ./ repmat(max(soundMatrix), size(soundMatrix, 1), 1);
         soundMatrix = soundMatrix';
-%         
-%         stdDevSound = std(soundMatrix);
-%         meanSound = mean(soundMatrix);
-% 
-%         averageSoundIndx = zeros(size(soundMatrix, 1));
-%         for n = 1:size(soundMatrix, 1)
-%             if (sum(abs(soundMatrix(n, :) - meanSound) <= 1.5*stdDevSound) == size(soundMatrix, 2))
-%                 averageSoundIndx(n) = 1;
-%             end
-%         end
-%         
-%         soundMatrix = soundMatrix(averageSoundIndx == 1, :);
         
         diffAmount = sum(var(soundMatrix));
         if diffAmount < leastDiffAmount
@@ -38,6 +26,6 @@ function [averageHarmonics, averageSound] = getAverageHarmonics(fileName, note)
     averageSound = averageSound ./ max(abs(averageSound));
     
     averageHarmonics = abs(fft(averageSound));
-    averageHarmonics = averageHarmonics ./ max(averageHarmonics);
     averageHarmonics = averageHarmonics(2:floor(length(averageHarmonics)/2));
+    averageHarmonics = averageHarmonics ./ max(averageHarmonics);
 end
