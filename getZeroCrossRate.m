b6a0062f2e43bab9a3aea6f06a0628bbaaa15c82
Saquib Hafiz <hafiz.saquib@gmail.com>
2016-03-19 20:30:58 -0400

    edit zcr

--- getZeroCrossRate.m
+++ getZeroCrossRate.m
@@ -1,15 +1,17 @@
 % Taken from http://www.mathworks.com/matlabcentral/fileexchange/19236-some-basic-audio-features/content/zcr.m
-function Z = zcr(signal,windowLength, step)
-    signal = signal / max(abs(signal));
+% and edited to suit our needs for our specifications as described in the
+% documentation
+
+function zeroCrossRate = getZeroCrossRate(signal,windowLength, step)
     curPos = 1;
     L = length(signal);
     numOfFrames = floor((L-windowLength)/step) + 1;
-    Z = zeros(numOfFrames,1);
+    zeroCrossRate = zeros(numOfFrames,1);
     for i = 1:numOfFrames
         window = (signal(curPos:curPos+windowLength-1));    
         window2 = zeros(size(window));
         window2(2:end) = window(1:end-1);
-        Z(i) = (1/(2*windowLength)) * sum(abs(sign(window)-sign(window2)));
+        zeroCrossRate(i) = (1/(2*windowLength)) * sum(abs(sign(window) - sign(window2)));
         curPos = curPos + step;
     end
 end
